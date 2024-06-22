631c1390fab353b82036bb9d1d1d87b0ebd3dc4f
DouglasKuerten <dk.kuerten.dk@hotmail.com>
2024-06-21 23:17:34 -0300

    Deployed 0e87c3c with MkDocs version: 1.6.0

--- geraDados/src/main/java/com/faker/geraDados/repository/PagamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.faker.geraDados.repository;
-
-import com.faker.geraDados.entity.Pagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-}
--- modelo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE imagems (
-	imagem bytea,
-	created_at timestamptz NOT NULL,
-	updated_at timestamptz NOT NULL
-);
